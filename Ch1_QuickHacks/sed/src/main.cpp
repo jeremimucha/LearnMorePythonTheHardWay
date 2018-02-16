8ef909feefb53ae8cba093e15408f2ff9c2876ae
Jeremi Mucha <jeremimucha@gmail.com>
2018-02-16 18:57:23 +0100

    Continue working on sed inplace fileupdate support

--- Ch1_QuickHacks/sed/src/main.cpp
+++ Ch1_QuickHacks/sed/src/main.cpp
@@ -6,6 +6,7 @@
 #include <iterator>
 #include <stdexcept>
 #include <boost/filesystem.hpp>
+#include <cstdio>   // for std::tmpnam
 #include "clara/clara.hpp"
 
 
@@ -36,6 +37,74 @@ struct bad_expression_exception : public std::runtime_error
     bad_expression_exception( const string& s ) : std::runtime_error(s) { };
 };
 
+struct bad_filename_exception : public std::runtime_error
+{
+    using std::runtime_error::runtime_error;
+};
+
+class fileupdate_base
+{
+public:
+    virtual ~fileupdate_base() noexcept = default;
+protected:
+    explicit fileupdate_base( const std::string& fname )
+        : filename{fname}
+        {
+            if( !fs::is_regular_file(filename) )
+                throw bad_filename_exception( fname + " is not a regular file" );
+        }
+    virtual void do_update( const std::regex& pattern,
+                            const std::string& replacement ) = 0;
+    static string make_temp( const string& fname ) 
+    {
+        return fname + std::tmpnam(nullptr);
+    }
+
+    fs::path filename;
+};
+
+class fileupdate_inplace : public fileupdate_base
+{
+public:
+    explicit fileupdate_inplace( const std::string& fname )
+        : fileupdate_base{fname}, ifs{fname}, ofs{ make_temp(fname) }
+        {
+            if( !ifs )
+                throw bad_filename_exception( "Failed to open the file " + fname );
+            if( !ofs )
+                throw bad_filename_exception( "Failed to create a temporary file" );
+        }
+    void operator()( const std::regex& pattern, const std::string& replacement );
+protected:
+    void do_update( const std::regex& pattern,
+                    const std::string& replacement ) override;
+private:
+    fs::ifstream ifs;
+    fs::ofstream ofs;
+};
+
+class fileupdate_backup : public fileupdate_base
+{
+public:
+    fileupdate_backup( const std::string& fname, const std::string& sfx )
+        : fileupdate_base{fname}
+        , ifs{filename}
+        , ofs{make_temp(fname)}
+        , backup_filename{fname+"."+sfx}
+        {
+            if( !ifs )
+                throw bad_filename_exception( "Failed to open the file " + fname );
+            if( !ofs )
+                throw bad_filename_exception( "Failed to create a temporary file" );
+            if( fs::exists(backup_filename) )
+                throw bad_filename_exception( "Invalid SUFFIX. File "
+                        + backup_filename.string() + " already exists." );
+        }
+private:
+    fs::ifstream ifs;
+    fs::ofstream ofs;
+    fs::path backup_filename;
+};
 
 auto replace_pattern( std::istream&, std::ostream&, const std::regex& re ) -> void;
 template<typename Container>
@@ -68,7 +137,7 @@ try{
         return 1;
     }
     auto pattern = std::regex( cli_args.pattern );
-    replace_in_files( cli_args.filenames, pattern, cli_args.replacement );
+    replace_in_files( cli_args.filenames, cout, pattern, cli_args.replacement );
 }
 catch( const std::exception& e )
 {
@@ -91,12 +160,13 @@ auto replace_pattern( std::istream& is, std::ostream& os,
 }
 
 template<typename Container>
-void replace_in_files( const Container& files, const std::regex& pattern,
-                       const std::string& replacement )
+void replace_in_files( const Container& files, std::ostream& os, 
+                    const std::regex& pattern, const std::string& replacement )
 {
     for( const auto& file : files ){
         auto ifs = std::ifstream{file};
         if( !ifs ) throw std::runtime_error( "Failed to open the file" + file );
-        replace_pattern( ifs, cout, pattern, replacement );
+        replace_pattern( ifs, os, pattern, replacement );
     }
 }
+
