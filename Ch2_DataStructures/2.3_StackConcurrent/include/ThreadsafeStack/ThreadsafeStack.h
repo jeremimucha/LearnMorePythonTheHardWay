09662c27148b72acf5e4a20e72a075c35a767759
Jeremi Mucha <jeremimucha@gmail.com>
2018-03-22 22:46:43 +0100

    Add ThreadsafeStack

---
+++ Ch2_DataStructures/2.3_StackConcurrent/include/ThreadsafeStack/ThreadsafeStack.h
@@ -0,0 +1,142 @@
+#ifndef STACK_CONCURRENT_GUARD_HPP_
+#define STACK_CONCURRENT_GUARD_HPP_
+
+#include <memory>
+#include <utility>
+#include <initializer_list>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#define GSL_THROW_ON_CONTRACT_VIOLATION
+#include <gsl/gsl>
+#if defined(NDEBUG)
+#undef Expects
+#undef Ensures
+#define Expects(cond)
+#define Ensures(cond)
+#endif
+
+using gsl::not_null;
+
+
+template<typename> class ThreadsafeStack;
+template<typename> class ThreadsafeStackNode;
+
+template<typename T>
+inline bool assert_invariant( const ThreadsafeStack<T>& );
+
+
+/* ThreadsafeStackNode */
+/* ------------------------------------------------------------------------- */
+template<typename T>
+struct ThreadsafeStackNode
+{
+    ThreadsafeStackNode() noexcept = default;
+    
+    template<typename... Args>
+    explicit ThreadsafeStackNode( Args&&... args )
+        : data{std::forward<Args>(args)...}
+        { }
+
+    T                    data;
+    ThreadsafeStackNode* next;
+};
+/* ------------------------------------------------------------------------- */
+
+/* ThreadsafeStack */
+/* ------------------------------------------------------------------------- */
+template<typename T>
+class ThreadsafeStack
+{
+    template<typename>
+    friend bool assert_invariant( const ThreadsafeStack<T>& );
+    using node = ThreadsafeStackNode<T>;
+    using Allocator = std::allocator<node>;
+    using T_Allocator = std::allocator<T>;
+    using alloc_traits = std::allocator_traits<Allocator>;
+    using T_alloc_traits = std::allocator_traits<T_Allocator>;
+    using mutex_type = std::mutex;
+    using condition_variable_type = std::condition_variable;
+    using self = ThreadsafeStack;
+public:
+    using allocator_type = T_Allocator;
+    using value_type = typename T_alloc_traits::value_type;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = typename T_alloc_traits::pointer;
+    using const_pointer = typename T_alloc_traits::const_pointer;
+    using difference_type = typename T_alloc_traits::difference_type;
+    using size_type = typename T_alloc_traits::difference_type;
+
+// --- No iterators by design
+    // using iterator = ThreadsafeStack_iterator<T>;
+    // using const_iterator = ThreadsafeStack_const_iterator<T>;
+    // using reverse_iterator = std::reverse_iterator<iterator>;
+    // using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+// --- constructors
+    ThreadsafeStack() noexcept;
+// --- No other than default constructor by design
+    // explicit ThreadsafeStack( size_type count )
+    //     : ThreadsafeStack(count, T{}) { }
+    
+    // ThreadsafeStack( size_type count, const T& value );
+
+// --- destructor
+    ~ThreadsafeStack() noexcept { free(); }
+
+// --- copy control
+    ThreadsafeStack( const ThreadsafeStack& ) = delete;
+    ThreadsafeStack( ThreadsafeStack&& ) noexcept = delete;
+    ThreadsafeStack& operator=( const ThreadsafeStack& ) = delete;
+    ThreadsafeStack& operator=( ThreadsafeStack&& ) noexcept = delete;
+
+// --- element access
+    // No element access methods returning references are provided. Doing so
+    // would expose the data beyond the control of the mutex.
+
+// --- capacity
+    bool empty() const noexcept { return top.next == nullptr; }
+
+// --- modifiers
+    void push( const T& value );
+    
+    void push( T&& value ) noexcept;
+    
+    template<typename... Args>
+    void emplace( Args&&... args );
+
+    bool try_pop( T& value ) noexcept;
+    value_type wait_and_pop() noexcept;
+    void wait_and_pop( T& value ) noexcept;
+
+    template<typename> friend inline 
+    void swap( ThreadsafeStack<T>& lhs, ThreadsafeStack<T>& rhs ) noexcept;
+
+protected:
+    template<typename... Args>
+    inline node* make_node( Args&&... args );
+
+    inline void link_nodes( not_null<node*> pred, node* succ ) noexcept;
+
+    inline node* unlink_top() noexcept;
+
+    inline void free() noexcept;
+    inline void free( not_null<node*> ) noexcept;
+    inline void free( node*, node* ) noexcept;
+
+private:
+    Allocator                       alloc{ Allocator{} };
+    mutable mutex_type              top_mutex{};
+    mutable condition_variable_type top_cond{};
+    node                            top;
+};
+/* ------------------------------------------------------------------------- */
+
+
+// --- implementation
+#include "ThreadsafeStack.impl.h"
+
+
+#endif /* STACK_CONCURRENT_GUARD_HPP_ */
---
+++ Ch2_DataStructures/2.3_StackConcurrent/include/ThreadsafeStack/ThreadsafeStack.impl.h
@@ -0,0 +1,134 @@
+#ifndef THREADSAFESTACK_IMPL_GUARD_HPP_
+#define THREADSAFESTACK_IMPL_GUARD_HPP_
+
+
+// --- helpers
+template<typename T>
+    template<typename... Args> inline
+auto ThreadsafeStack<T>::make_node( Args&&... args ) -> node*
+{
+    auto nn = alloc.allocate(1);
+    alloc.construct(nn, std::forward<Args>(args)...);
+    return nn;
+}
+
+template<typename T> inline
+void ThreadsafeStack<T>::link_nodes( not_null<node*> pred, node* succ ) noexcept
+{
+    pred->next = succ;
+}
+
+template<typename T> inline
+void ThreadsafeStack<T>::free() noexcept
+{
+    std::lock_guard<mutex_type> lk{top_mutex};
+    while(top.next != nullptr){
+        auto target = top.next;
+        top.next = target->next;
+        free(target);
+    }
+}
+
+template<typename T> inline
+void ThreadsafeStack<T>::free( not_null<node*> target ) noexcept
+{
+    alloc.destroy(target);
+    alloc.deallocate(target, 1);
+}
+
+template<typename T> inline
+void ThreadsafeStack<T>::free( node* first, node* last ) noexcept
+{
+    while( first != last ){
+        auto temp = first;
+        first = first->next;
+        free(temp);
+    }
+}
+
+
+// --- modifiers
+template<typename T>
+void ThreadsafeStack<T>::push( const T& value )
+{
+    auto nn = make_node(value);
+    std::lock_guard<mutex_type> lk{top_mutex};
+    link_nodes(nn, top.next);
+    link_nodes(top.next, nn);
+}
+
+template<typename T>
+void ThreadsafeStack<T>::push( T&& value ) noexcept
+{
+    auto nn = make_node(std::move(value));
+    std::lock_guard<mutex_type> lk{top_mutex};
+    link_nodes(nn, top.next);
+    link_nodes(top.next, nn);
+}
+
+template<typename T>
+    template<typename... Args>
+void ThreadsafeStack<T>::emplace( Args&&... args )
+{
+    auto nn = make_node(std::forward<Args>(args)...);
+    std::lock_guard<mutex_type> lk{top_mutex};
+    link_nodes(nn, top.next);
+    link_nodes(top.next, nn);
+}
+
+template<typename T>
+bool ThreadsafeStack<T>::try_pop( T& value ) noexcept
+{
+    if( empty() )
+        return false;
+    auto target = unlink_top();
+    value = std::move(target->data);
+    return true;
+}
+
+template<typename T> // TODO: enable_if noexcept move constructible
+auto ThreadsafeStack<T>::wait_and_pop() noexcept -> value_type
+{ // This implementation requires type T to be noexcept move-constructible
+    std::unique_lock<mutex_type> lk{top_mutex};
+    top_cond.wait( lk, [this]{return !empty();} );
+    auto target = top.next;
+    link_nodes(top.next, target->next);
+    lk.unlock();
+    return std::move(target->data);
+}
+
+template<typename T> // TODO: enable_if noexcept move assignable
+void ThreadsafeStack<T>::wait_and_pop( T& value ) noexcept
+{ // This implementation requires type T to be noexcept move-assignable
+    std::unique_lock<mutex_type> lk{top_mutex};
+    top_cond.wait( lk, [this]{return !empty();} );
+    auto target = top.next;
+    link_nodes(top.next, target->next);
+    lk.unlock();
+    value = std::move(target->data);
+}
+
+template<typename T>
+auto ThreadsafeStack<T>::unlink_top() noexcept -> node*
+{ Expects(!empty());
+    std::lock_guard<mutex_type> lk{top_mutex};
+    auto target = top.next;
+    link_nodes(top.next, target->next);
+    // link_nodes(target.next, nullptr); // necessary?
+    return target;
+}
+
+
+template<typename T>
+inline void swap( ThreadsafeStack<T>& lhs, ThreadsafeStack<T>& rhs ) noexcept
+{
+    using std::swap;
+    using mutex_type = typename ThreadsafeStack<T>::mutex_type;
+    std::lock(lhs.top_mutex, rhs.top_mutex);
+    std::lock_guard<mutex_type> lhslk{lhs.top_mutex, std::adopt_lock};
+    std::lock_guard<mutex_type> rhslk{rhs.top_mutex, std::adopt_lock};
+    swap(lhs.alloc, rhs.alloc);
+    swap(lhs.top, rhs.top);
+}
+
+#endif /* THREADSAFESTACK_IMPL_GUARD_HPP_ */
