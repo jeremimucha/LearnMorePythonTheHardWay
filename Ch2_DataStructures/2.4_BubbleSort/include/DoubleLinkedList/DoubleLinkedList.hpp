594352b1905967d9c3bc307e362e94f1838bd112
Jeremi Mucha <jeremimucha@gmail.com>
2018-04-02 09:14:44 +0200

    Implement bubble sort algorithm for double linked list

---
+++ Ch2_DataStructures/2.4_BubbleSort/include/DoubleLinkedList/DoubleLinkedList.hpp
@@ -0,0 +1,441 @@
+#ifndef DOUBLELINKEDLIST_HPP_
+#define DOUBLELINKEDLIST_HPP_
+
+#include <memory>
+#include <utility>
+#include <algorithm>
+#include <iterator>
+#include <initializer_list>
+#include <type_traits>
+
+#define GSL_THROW_ON_CONTRACT_VIOLATION
+#include <gsl/gsl>
+#if defined(NDEBUG)
+#undef Expects
+#undef Ensures
+#define Expects(cond)
+#define Ensures(cond)
+#endif
+
+using gsl::not_null;
+using gsl::owner;
+
+
+template<typename> class DoubleLinkedList;
+
+
+namespace detail
+{
+
+template<typename> class Node;
+template<typename> class DLL_iterator;
+template<typename> class DLL_const_iterator;
+
+template<typename U>
+bool ensure_invariant( const DoubleLinkedList<U>& );
+/* DLL Node<T> */
+/* ------------------------------------------------------------------------- */
+template<typename T>
+struct Node
+{
+friend class DoubleLinkedList<T>;
+friend class DLL_iterator<T>;
+friend class DLL_const_iterator<T>;
+template<typename U> friend bool ensure_invariant( const DoubleLinkedList<U>& );
+
+    Node() noexcept = default;
+    template<typename... Args>
+    explicit Node( Args&&... args )
+        : data{std::forward<Args>(args)...}
+        { }
+
+    T       data;
+    Node*   next{nullptr};
+    Node*   prev{nullptr};
+};
+/* ------------------------------------------------------------------------- */
+
+} // detail
+
+
+/* DoubleLinkedList */
+/* ------------------------------------------------------------------------- */
+template<typename T>
+class DoubleLinkedList
+{
+    template<typename U>
+    friend bool detail::ensure_invariant( const DoubleLinkedList<U>& );
+    using node                      = detail::Node<T>;
+    using Allocator                 = std::allocator<node>;
+    using T_Allocator               = std::allocator<T>;
+    using alloc_traits              = std::allocator_traits<Allocator>;
+    using T_alloc_traits            = std::allocator_traits<T_Allocator>;
+    using self                      = DoubleLinkedList;
+public:
+    using allocator_type            = T_Allocator;
+    using value_type                = typename T_alloc_traits::value_type;
+    using reference                 = value_type&;
+    using const_reference           = const value_type&;
+    using pointer                   = typename T_alloc_traits::pointer;
+    using const_pointer             = typename T_alloc_traits::const_pointer;
+    using difference_type           = typename T_alloc_traits::difference_type;
+    using size_type                 = typename T_alloc_traits::size_type;
+
+    using iterator                  = detail::DLL_iterator<T>;
+    using const_iterator            = detail::DLL_const_iterator<T>;
+    using reverse_iterator          = std::reverse_iterator<iterator>;
+    using const_reverse_iterator    = std::reverse_iterator<const_iterator>;
+
+
+// --- constructors
+    DoubleLinkedList() noexcept
+    {
+        link_nodes(&head, &tail);
+    }
+    explicit DoubleLinkedList( size_type count )
+        : DoubleLinkedList(count, T{}) { }
+    DoubleLinkedList( size_type count, const T& value )
+    {
+        auto nodes = alloc_n(count, value);
+        link_nodes(&head, nodes, &tail);
+    }
+    DoubleLinkedList( std::initializer_list<T> ilist )
+    {
+        auto nodes = alloc_range(ilist.begin(), ilist.end());
+        link_nodes(&head, nodes, &tail);
+    }
+
+// --- destructor
+    ~DoubleLinkedList() noexcept { free(); }
+
+// --- copy control
+    DoubleLinkedList( const DoubleLinkedList& other )
+        : alloc{other.alloc}
+    {
+        auto nodes = alloc_range(other.cbegin(), other.cend());
+        link_nodes(&head, nodes, &tail);
+    }
+    DoubleLinkedList( DoubleLinkedList&& other ) noexcept
+        : alloc{std::move(other.alloc)}
+    {
+        link_nodes(&head, {other.head.next, other.tail.prev}, &tail);
+        link_nodes(&other.head, &other.tail);
+    }
+    DoubleLinkedList& operator=( const DoubleLinkedList& other );
+    DoubleLinkedList& operator=( DoubleLinkedList&& other ) noexcept;
+    DoubleLinkedList& operator=( std::initializer_list<T> ilist );
+
+// --- element access
+    reference front()
+    { Expects(head.next != &tail);
+        return head.next->data;
+    }
+    const_reference front() const
+    { Expects(head.next != &tail);
+        return head.next->data;
+    }
+    reference back()
+    { Expects(tail.prev != &head);
+        return tail.prev->data;
+    }
+    const_reference back() const
+    { Expects(tail.prev != &head);
+        return tail.prev->data;
+    }
+
+// --- iterators
+    iterator begin() noexcept { return iterator{head.next}; }
+    const_iterator begin() const noexcept { return cbegin(); }
+    const_iterator cbegin() const noexcept { return const_iterator{head.next}; }
+    iterator end() noexcept { return iterator{&tail}; }
+    const_iterator end() const noexcept { return cend(); }
+    const_iterator cend() const noexcept { return const_iterator{&tail}; }
+    reverse_iterator rbegin() noexcept { return reverse_iterator{begin()}; }
+    const_reverse_iterator rbegin() const noexcept { return crbegin(); }
+    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator{cbegin()}; }
+    reverse_iterator rend() noexcept { return reverse_iterator{end()}; }
+    const_reverse_iterator rend() const noexcept { return crend(); }
+    const_reverse_iterator crend() const noexcept { return const_reverse_iterator{cend()}; }
+
+// --- capacity
+    bool empty() const noexcept { return head.next == &tail; }
+
+    size_type size() const noexcept
+    { return std::count_if(cbegin(),cend(),[](const_reference){return true;}); }
+
+// --- modifiers
+    void clear() noexcept { free(); }
+
+    iterator insert( const_iterator pos, const T& value );
+
+    iterator insert( const_iterator pos, T&& value );
+
+    template<typename InputIt
+            , typename = std::enable_if_t<
+                std::is_base_of_v<std::input_iterator_tag,
+                    typename std::iterator_traits<InputIt>::iterator_category
+                >
+              >
+            >
+    iterator insert( const_iterator pos, InputIt first, InputIt last );
+
+    iterator insert( const_iterator pos, std::initializer_list<T> ilist );
+
+    template<typename... Args>
+    iterator emplace( const_iterator pos, Args&&... args );
+
+    iterator erase( const_iterator pos );
+
+    iterator erase( const_iterator first, const_iterator last );
+
+    void push_back( const T& value );
+
+    void push_back( T&& value );
+
+    template<typename... Args>
+    reference emplace_back( Args&&... args );
+
+    void pop_back();
+
+    void push_front( const T& value );
+
+    void push_front( T&& value );
+
+    template<typename... Args>
+    reference emplace_front( Args&&... args );
+
+    void pop_front();
+
+    void resize( size_type count );
+    void resize( size_type count, const T& value );
+
+    void sort_bubble() noexcept;
+    void sort_merge() noexcept;
+
+    void swap( DoubleLinkedList& other ) noexcept
+    {
+        using std::swap;
+        swap(alloc, other.alloc);
+        swap(head, other.head);
+        swap(tail, other.tail);
+    }
+
+    friend inline void swap( DoubleLinkedList& lhs, DoubleLinkedList& rhs ) noexcept
+    {
+        using std::swap;
+        swap(lhs.alloc, rhs.alloc);
+        swap(lhs.head, rhs.head);
+        swap(lhs.tail, rhs.tail);
+    }
+
+protected:
+
+    template<typename... Args>
+    inline node* make_node( Args&&... args );
+
+    inline void link_nodes( not_null<node*> pred, not_null<node*> succ ) noexcept;
+
+    inline void link_nodes( not_null<node*> pred
+                          , std::pair<node*,node*> range
+                          , not_null<node*> succ ) noexcept;
+
+    template<typename... Args> inline
+    std::pair<node*,node*> alloc_n( size_type n, Args&&... args );
+
+    template<typename InputIt> inline
+    std::pair<node*,node*> alloc_range( InputIt begin, InputIt end );
+
+    inline void swap_nodes( node*, node* ) noexcept;
+
+    inline void free() noexcept;
+
+    inline void free( node* first, node* last ) noexcept;
+
+    inline void free( node* n ) noexcept;
+private:
+    Allocator alloc{Allocator()};
+    node head{};
+    node tail{};
+};
+/* ------------------------------------------------------------------------- */
+
+namespace detail
+{
+
+class Invariant_violation_exception : public std::logic_error
+{
+    using std::logic_error::logic_error;
+};
+
+template<typename T>
+bool ensure_invariant( const DoubleLinkedList<T>& object )
+{
+    if( object.head.next == nullptr || object.tail.prev == nullptr )
+        throw Invariant_violation_exception("Head or tail point to a nullptr");
+    if( (object.head.next == &object.tail &&
+         object.tail.prev == &object.head)  // empty list
+        ||
+        (object.head.next->prev == &object.head &&
+         object.tail.prev->next == &object.tail) ) // non-empty list
+        return true;
+    else
+        throw Invariant_violation_exception("Empty or non-empty invariant violated");
+}
+
+} // detail
+
+namespace detail
+{
+/* DLL_iterator<T> */
+/* ------------------------------------------------------------------------- */
+template<typename T>
+class DLL_iterator
+{
+    friend class DoubleLinkedList<T>;
+    using Container                     = DoubleLinkedList<T>;
+    using node                          = Node<T>;
+    using self                          = DLL_iterator;
+public:
+    using value_type                    = typename Container::value_type;
+    using reference                     = typename Container::reference;
+    using pointer                       = typename Container::pointer;
+    using iterator_category             = std::bidirectional_iterator_tag;
+    using difference_type               = typename Container::difference_type;
+    using const_iterator                = DLL_const_iterator<T>;
+
+    explicit DLL_iterator( node* n ) noexcept
+        : base{n} { }
+
+    reference operator*() const noexcept
+        { return base->data; }
+
+    pointer operator->() const noexcept
+        { return &(base->data); }
+
+    self& operator++() noexcept
+        {
+            base = base->next;
+            return *this;
+        }
+    
+    self operator++(int) noexcept
+        {
+            auto rv = *this;
+            base = base->next;
+            return rv;
+        }
+
+    self& operator--() noexcept
+        {
+            base = base->prev;
+            return *this;
+        }
+
+    self operator--(int) noexcept
+        {
+            auto rv = *this;
+            base = base->prev;
+            return rv;
+        }
+
+    operator const_iterator() { return const_iterator(base); }
+
+template<typename U>
+friend bool operator==(const DLL_const_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_const_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator==(const DLL_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator==(const DLL_const_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_const_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+template<typename U>
+friend bool operator==(const DLL_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+private:
+    node* base;
+};
+/* ------------------------------------------------------------------------- */
+
+/* DLL_const_iterator<T> */
+/* ------------------------------------------------------------------------- */
+template<typename T>
+class DLL_const_iterator
+{
+    friend class DoubleLinkedList<T>;
+    using Container                     = DoubleLinkedList<T>;
+    using node                          = Node<T>;
+    using self                          = DLL_const_iterator;
+public:
+    using value_type                    = typename Container::value_type;
+    using reference                     = typename Container::const_reference;
+    using pointer                       = typename Container::const_pointer;
+    using iterator_category             = std::bidirectional_iterator_tag;
+    using difference_type               = typename Container::difference_type;
+
+    explicit DLL_const_iterator( const node* n ) noexcept
+        : base{n} { }
+
+    reference operator*() const noexcept
+        { return base->data; }
+
+    pointer operator->() const noexcept
+        { return &(base->data); }
+
+    self& operator++() noexcept
+        {
+            base = base->next;
+            return *this;
+        }
+    
+    self operator++(int) noexcept
+        {
+            auto rv = *this;
+            base = base->next;
+            return rv;
+        }
+
+    self& operator--() noexcept
+        {
+            base = base->prev;
+            return *this;
+        }
+
+    self operator--(int) noexcept
+        {
+            auto rv = *this;
+            base = base->prev;
+            return rv;
+        }
+    
+template<typename U>
+friend bool operator==(const DLL_const_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_const_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator==(const DLL_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_iterator<U>& lhs, const DLL_const_iterator<U>& rhs);
+template<typename U>
+friend bool operator==(const DLL_const_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_const_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+template<typename U>
+friend bool operator==(const DLL_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+template<typename U>
+friend bool operator!=(const DLL_iterator<U>& lhs, const DLL_iterator<U>& rhs);
+
+private:
+    const node* base;
+};
+
+} // detail
+
+#include "DoubleLinkedList_impl.hpp"
+
+/* ------------------------------------------------------------------------- */
+#endif /* DOUBLELINKEDLIST_HPP_ */
---
+++ Ch2_DataStructures/2.4_BubbleSort/tests/test_modifiers.cpp
@@ -0,0 +1,366 @@
+#include "gtest/gtest.h"
+#include "DoubleLinkedList/DoubleLinkedList.hpp"
+#include <algorithm>
+#include <iterator>
+#include <chrono>
+#include <random>
+#include <iostream>
+
+
+namespace
+{
+
+using detail::ensure_invariant;
+std::uniform_int_distribution<> ud{0,100};
+std::default_random_engine re{
+    static_cast<unsigned int>(std::chrono::steady_clock::now().time_since_epoch().count())};
+
+
+class ModifiersTest : public ::testing::Test
+{
+    void SetUp() override
+    {
+        ensure_invariant(f_dll);
+    }
+protected:
+    using size_type = typename DoubleLinkedList<int>::size_type;
+    DoubleLinkedList<int> f_dll{0,1,2,3,4,5,6,7,8,9};
+    const size_type f_original_size{f_dll.size()};
+};
+
+class SortTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        for(auto i=0; i<20; ++i){
+            f_random_list.push_back(ud(re));
+        }
+        ASSERT_TRUE(ensure_invariant(f_empty_list));
+        ASSERT_TRUE(ensure_invariant(f_single_elem_list));
+        ASSERT_TRUE(ensure_invariant(f_random_list));
+        ASSERT_TRUE(f_empty_list.empty());
+        ASSERT_FALSE(f_single_elem_list.empty());
+        ASSERT_FALSE(f_random_list.empty());
+    }
+
+    DoubleLinkedList<int> f_empty_list{};
+    DoubleLinkedList<int> f_single_elem_list{42};
+    DoubleLinkedList<int> f_two_elem_list{42, 11};
+    DoubleLinkedList<int> f_random_list{};
+};
+
+std::ostream& operator<<(std::ostream& os, const DoubleLinkedList<int>& list)
+{
+    for( const auto i : list )
+        os << i << " ";
+    return os;
+}
+
+TEST_F( ModifiersTest, ConstLvalue )
+{
+    const int value = 42;
+    f_dll.insert(f_dll.cbegin(), value);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.front(), value);
+    
+    f_dll.insert(f_dll.cend(), value);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.back(), value);
+    
+    auto it = f_dll.begin();
+    std::advance(it, 5);
+    auto inserted = f_dll.insert(it, value);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(--it, inserted);
+    ASSERT_EQ(*inserted, value);
+}
+
+TEST_F( ModifiersTest, Rvalue )
+{
+    f_dll.insert(f_dll.cbegin(), 42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.front(), 42);
+    
+    f_dll.insert(f_dll.cend(), 42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.back(), 42);
+    
+    auto it = f_dll.begin();
+    std::advance(it, 5);
+    auto inserted = f_dll.insert(it, 42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(--it, inserted);
+    ASSERT_EQ(*inserted, 42);
+}
+
+TEST_F( ModifiersTest, Range )
+{
+    const auto range = std::vector<int>{99,88,77,66,55,44,33};
+    auto it1 = f_dll.insert(f_dll.cbegin(), range.cbegin(), range.cend());
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_TRUE(std::equal(range.cbegin(),range.cend(),it1));
+    
+    auto it2 = f_dll.insert(f_dll.cend(), range.cbegin(), range.cend());
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_TRUE(std::equal(range.cbegin(),range.cend(),it2));
+    
+    auto ipoint = f_dll.begin();
+    std::advance(ipoint, 5);
+    auto it3 = f_dll.insert(ipoint, range.cbegin(), range.cend());
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    std::advance(ipoint, -range.size());
+    ASSERT_EQ(ipoint, it3);
+    ASSERT_TRUE(std::equal(range.cbegin(),range.cend(),it3));
+
+    ASSERT_EQ(f_dll.size(), f_original_size + range.size()*3);
+}
+
+TEST_F( ModifiersTest, InitializerList )
+{
+    constexpr auto ilist = {99,88,77,66,55,44,44,22,11};
+    const auto ilist_size = std::distance(ilist.begin(),ilist.end());
+
+    auto it1 = f_dll.insert(f_dll.cbegin(), {99,88,77,66,55,44,44,22,11});
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_TRUE(std::equal(ilist.begin(), ilist.end(), it1));
+
+    auto it2 = f_dll.insert(f_dll.cend(), {99,88,77,66,55,44,44,22,11});
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_TRUE(std::equal(ilist.begin(),ilist.end(),it2));
+
+    auto ipoint = f_dll.begin();
+    std::advance(ipoint, f_original_size/2 + ilist_size);
+    auto it3 = f_dll.insert(ipoint, {99,88,77,66,55,44,44,22,11});
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    std::advance(ipoint, -ilist_size);
+    ASSERT_EQ(ipoint, it3);
+    ASSERT_TRUE(std::equal(ilist.begin(), ilist.end(), it3));
+}
+
+TEST_F( ModifiersTest, Emplace )
+{
+    auto lvalue = 11;
+    const auto clvalue = 42;
+
+    auto it1 = f_dll.emplace(f_dll.cbegin(), lvalue);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(*it1, lvalue);
+
+    auto it2 = f_dll.emplace(f_dll.cend(), clvalue);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(*it2, clvalue);
+
+    auto ipoint = f_dll.begin();
+    std::advance(ipoint, f_dll.size()/2);
+    auto it3 = f_dll.emplace(ipoint, lvalue+clvalue);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    std::advance(ipoint,-1);
+    ASSERT_EQ(ipoint, it3);
+    ASSERT_EQ(*ipoint, *it3);
+    ASSERT_EQ(*it3, lvalue+clvalue);
+}
+
+TEST_F( ModifiersTest, EraseSingleFirst )
+{
+    const auto expected = *++f_dll.cbegin();
+    auto it = f_dll.erase(f_dll.cbegin());
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(expected,*it);
+    ASSERT_EQ(f_dll.size(), f_original_size-1);
+}
+
+// Can't use end() iterator. How do we test that?
+// TEST_F( ModifiersTest, EraseSingleLast )
+// {
+
+// }
+
+TEST_F( ModifiersTest, EraseRange )
+{
+    auto begin = f_dll.cbegin();
+    std::advance(begin, 1);
+    auto end = f_dll.cend();
+    std::advance(end, -1);
+    const auto range_size = std::distance(begin, end);
+
+    auto it = f_dll.erase(begin, end);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(it, end);
+    ASSERT_EQ(f_dll.size(), f_original_size - range_size);
+}
+
+TEST_F( ModifiersTest, PushBackLvalue )
+{
+    const auto value = 42;
+    f_dll.push_back(value);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), f_original_size+1);
+    ASSERT_EQ(value, f_dll.back());
+}
+
+TEST_F( ModifiersTest, PushBackRvalue )
+{
+    f_dll.push_back(42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), f_original_size+1);
+    ASSERT_EQ(f_dll.back(), 42);
+}
+
+TEST_F( ModifiersTest, EmplaceBack )
+{
+    f_dll.push_back(42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), f_original_size+1);
+    ASSERT_EQ(f_dll.back(), 42);
+}
+
+TEST_F( ModifiersTest, PopBack )
+{
+    const auto second_to_last = [&f_dll = f_dll]{ return *----f_dll.cend(); }();
+    f_dll.pop_back();
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), f_original_size-1);
+    ASSERT_EQ(second_to_last, f_dll.back());
+}
+
+TEST_F( ModifiersTest, PushFrontLvalue )
+{
+    const auto value = 42;
+    f_dll.push_front(value);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.front(), value);
+    ASSERT_EQ(f_dll.size(), f_original_size + 1);
+}
+
+TEST_F( ModifiersTest, PushFrontRvalue )
+{
+    f_dll.push_front(42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.front(), 42);
+    ASSERT_EQ(f_dll.size(), f_original_size + 1);
+}
+
+TEST_F( ModifiersTest, EmplaceFront )
+{
+    f_dll.emplace_front(42);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.front(), 42);
+    ASSERT_EQ(f_dll.size(), f_original_size + 1);
+}
+
+TEST_F( ModifiersTest, PopFront )
+{
+    const auto second = [&f_dll = f_dll]{ return *++f_dll.cbegin(); }();
+    f_dll.pop_front();
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), f_original_size - 1);
+    ASSERT_EQ(second, f_dll.front());
+}
+
+TEST_F( ModifiersTest, ResizeSmaller )
+{
+    const auto desired_count = 5;
+    f_dll.resize(desired_count);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), desired_count);
+}
+
+TEST_F( ModifiersTest, ResizeBigger )
+{
+    const auto desired_count = 42;
+    f_dll.resize(desired_count);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), desired_count);
+}
+
+TEST_F( ModifiersTest, ResizeEmpty )
+{
+    auto dll = DoubleLinkedList<int>{};
+    const auto original_size = dll.size();
+    ASSERT_EQ(original_size, 0);
+    const auto desired_count = 42;
+    dll.resize(desired_count, 42);
+    ASSERT_TRUE(ensure_invariant(dll));
+    ASSERT_EQ(dll.size(), desired_count);
+}
+
+TEST_F( ModifiersTest, ResizeToEmpty )
+{
+    const auto desired_count = 0;
+    f_dll.resize(desired_count);
+    ASSERT_TRUE(ensure_invariant(f_dll));
+    ASSERT_EQ(f_dll.size(), desired_count);
+}
+
+TEST_F( SortTest, BubbleSortEmptyList )
+{
+    ASSERT_TRUE(std::is_sorted(f_empty_list.cbegin(), f_empty_list.cend()));
+    f_empty_list.sort_bubble();
+    ASSERT_TRUE(ensure_invariant(f_empty_list));
+    ASSERT_TRUE(std::is_sorted(f_empty_list.cbegin(), f_empty_list.cend()));
+}
+
+TEST_F( SortTest, BubbleSortSingleElem )
+{
+    ASSERT_TRUE(std::is_sorted(f_single_elem_list.cbegin(),
+                    f_single_elem_list.cend()));
+    f_single_elem_list.sort_bubble();
+    ASSERT_TRUE(ensure_invariant(f_single_elem_list));
+    ASSERT_TRUE(std::is_sorted(f_single_elem_list.cbegin(),
+                    f_single_elem_list.cend()));
+}
+
+TEST_F( SortTest, BubbleSortRandomList )
+{
+    ASSERT_FALSE(std::is_sorted(f_random_list.cbegin(),
+                    f_random_list.cend()));
+    f_random_list.sort_bubble();
+    ASSERT_TRUE(ensure_invariant(f_random_list));
+    ASSERT_TRUE(std::is_sorted(f_random_list.cbegin(),
+                    f_random_list.cend()));
+}
+
+TEST_F( SortTest, MergeSortEmptyList )
+{
+    ASSERT_TRUE(std::is_sorted(f_empty_list.cbegin(), f_empty_list.cend()));
+    f_empty_list.sort_merge();
+    ASSERT_TRUE(ensure_invariant(f_empty_list));
+    ASSERT_TRUE(std::is_sorted(f_empty_list.cbegin(), f_empty_list.cend()));
+}
+
+TEST_F( SortTest, MergeSortSingleElem )
+{
+    ASSERT_TRUE(std::is_sorted(f_single_elem_list.cbegin(),
+                    f_single_elem_list.cend()));
+    f_single_elem_list.sort_merge();
+    ASSERT_TRUE(ensure_invariant(f_single_elem_list));
+    ASSERT_TRUE(std::is_sorted(f_single_elem_list.cbegin(),
+                    f_single_elem_list.cend()));
+}
+
+TEST_F( SortTest, MergeSortTwoElem )
+{
+    ASSERT_FALSE(std::is_sorted(f_two_elem_list.cbegin(),
+                    f_two_elem_list.cend()));
+    std::cout << "Before sort:\n\t" << f_two_elem_list << "\n";
+    f_two_elem_list.sort_merge();
+    std::cout << "After sort:\n\t" << f_two_elem_list << "\n";
+    ASSERT_TRUE(ensure_invariant(f_two_elem_list));
+    ASSERT_TRUE(std::is_sorted(f_two_elem_list.cbegin(),
+                    f_two_elem_list.cend()));
+}
+
+// TEST_F( SortTest, MergeSortRandomList )
+// {
+//     ASSERT_FALSE(std::is_sorted(f_random_list.cbegin(),
+//                     f_random_list.cend()));
+//     std::cout << "Before sort:\n\t" << f_random_list << "\n";
+//     f_random_list.sort_merge();
+//     std::cout << "After sort:\n\t" << f_random_list << "\n";
+//     ASSERT_TRUE(ensure_invariant(f_random_list));
+//     ASSERT_TRUE(std::is_sorted(f_random_list.cbegin(),
+//                     f_random_list.cend()));
+// }
+
+} // namespace
